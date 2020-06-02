e0f79c585b0788ab2f481b8b0ce248c493090376
LiviaSibi <livia.sibinelli@gmail.com>
2020-06-02 12:09:35 -0300

    add entregas

---
+++ Senai_InLock_Livia_Tais_Entrega/Senai.InLock.WebApi/Senai.InLock.WebApi/Controller/EstudioController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.InLock.WebApi.Domains;
+using Senai.InLock.WebApi.Interfaces;
+using Senai.InLock.WebApi.Repository;
+
+namespace Senai.InLock.WebApi.Controller
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    [Authorize(Roles = "1")]
+    public class EstudioController : ControllerBase
+    {
+        private IEstudioRepository _estudioRepository { get; set; }
+
+        public EstudioController()
+        {
+            _estudioRepository = new EstudioRepository();
+        }
+
+        /// <summary>
+        /// Lista todos os estúdios
+        /// </summary>
+        /// <returns>Retorna todos os estudios</returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public IEnumerable<EstudioDomain> Get()
+        {
+            return _estudioRepository.Listar();
+        }
+
+        /// <summary>
+        /// Cadastra um novo Estudio
+        /// </summary>
+        /// <param name="novoEstudio">Nome do novoEstudio que será cadastrado</param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public IActionResult Cadastrar(EstudioDomain novoEstudio)
+        {
+            _estudioRepository.Cadastrar(novoEstudio);
+            return StatusCode(201);
+        }
+    }
+}
---
+++ Senai_InLock_Livia_Tais_Entrega/Senai.InLock.WebApi/Senai.InLock.WebApi/Controller/UsuarioController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.InLock.WebApi.Domains;
+using Senai.InLock.WebApi.Interfaces;
+using Senai.InLock.WebApi.Repository;
+
+namespace Senai.InLock.WebApi.Controller
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository { get; set; }
+
+        public UsuarioController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        /// <summary>
+        /// Lista todos os Usuarios
+        /// </summary>
+        /// <returns>Retorna todos os usuarios cadastrados</returns>
+        [Authorize(Roles ="1")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public IEnumerable<UsuarioDomain> Get()
+        {
+            return _usuarioRepository.Listar();
+        }
+
+        /// <summary>
+        /// Cadastra um novo usuário
+        /// </summary>
+        /// <param name="novoUsuario">Nome do novoUsuario que será cadastrado</param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public IActionResult Cadastrar(UsuarioDomain novoUsuario)
+        {
+            _usuarioRepository.Cadastrar(novoUsuario);
+            return StatusCode(201);
+        }
+    }
+}
